8f4251b567a71d6ad1ce8483fc1effd3f2851275
blackmatch <coder.blackmatch@gmail.com>
2018-11-19 22:51:48 +0800

    perf: two sum

--- JavaScript/TwoSum/index.js
+++ JavaScript/TwoSum/index.js
@@ -1,11 +1,11 @@
 const debug = require('debug')('twoSum');
 
-const nums = [2, 7, 11, 15];
-const target = 9;
+const nums = [3, 2, 4];
+const target = 6;
 
 // solution1 -- O(n^2)
 const solution1 = (ns, t) => {
-  for (let i = 0, iLen = ns.length - 1; i < iLen; i += 1) {
+  for (let i = 0, iLen = ns.length; i < iLen; i += 1) {
     for (let j = i + 1, jLen = ns.length; j < jLen; j += 1) {
       if (ns[i] + ns[j] === t) {
         return [i, j];
@@ -13,29 +13,72 @@ const solution1 = (ns, t) => {
     }
   }
 
-  return [];
+  throw new Error('No tow sum solution');
 };
 
 debug('solution1:');
 const result1 = solution1(nums, target);
 debug(result1);
 
+
 // solution2 -- O(n)
 const solution2 = (ns, t) => {
-  const ts = [];
   for (let i = 0, iLen = ns.length; i < iLen; i += 1) {
     const n = t - ns[i];
     const idx = ns.indexOf(n);
     if (idx !== -1 && idx !== i) { // attention
-      ts.push(i);
-      ts.push(idx);
-      break;
+      return [i, idx];
     }
   }
 
-  return ts;
+  throw new Error('No tow sum solution');
 };
 
 debug('solution2:');
 const result2 = solution2(nums, target);
 debug(result2);
+
+
+// solution3 -- O(n)
+// trading space for speed
+const solution3 = (ns, t) => {
+  const hashMap = {};
+  for (let i = 0, iLen = ns.length; i < iLen; i += 1) {
+    hashMap[`${ns[i]}`] = i;
+  }
+
+  for (let i = 0, iLen = ns.length; i < iLen; i += 1) {
+    const complement = t - ns[i];
+    if (hashMap[`${complement}`] !== undefined
+    && hashMap[`${complement}`] !== i) {
+      return [i, hashMap[`${complement}`]];
+    }
+  }
+
+  throw new Error('No tow sum solution');
+};
+
+debug('solution3:');
+const result3 = solution3(nums, target);
+debug(result3);
+
+
+// solution4 -- O(n)
+const solution4 = (ns, t) => {
+  const hashMap = {};
+
+  for (let i = 0, iLen = ns.length; i < iLen; i += 1) {
+    const complement = t - ns[i];
+    if (hashMap[`${complement}`] !== undefined) {
+      return [hashMap[`${complement}`], i];
+    }
+
+    hashMap[`${ns[i]}`] = i;
+  }
+
+  throw new Error('No tow sum solution');
+};
+
+debug('solution4:');
+const result4 = solution4(nums, target);
+debug(result4);
